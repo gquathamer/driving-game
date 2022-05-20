d5ae3e2b6fb4cf9ad2f641cd9291398d1b8a5245
Garrett Quathamer <gquathamer@gmail.com>
2022-05-20 04:39:31 +0000

    added ability to move on spacebar press, need to work on stopping on keyup next

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 var data = {
-  direction: 'east'
+  direction: 'east',
+  positionX: 0,
+  positionY: 0,
 }
 
 var carContainer = document.querySelector('.car-container');
 var carImage = document.querySelector('img');
+
+data.positionX = carContainer.left;
+data.positionY = carContainer.top;
 
 document.addEventListener('keydown', function (event) {
   if (event.keyCode === 37) {
@@ -18,5 +23,14 @@ document.addEventListener('keydown', function (event) {
   } else if (event.keyCode === 40) {
     carImage.className = 'car-south';
     data.direction = 'south';
+  } else if (event.keyCode === 32) {
+    var num = 5;
+    var interval = setInterval(function () {
+      carContainer.style.left = (num.toString() + 'px');
+      data.positionX = carContainer.left;
+      carContainer.style.top = (num.toString() + 'px');
+      data.positionY = carContainer.top;
+      num++;
+    }, 16);
   }
 })
